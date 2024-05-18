750b50e8298f8d5b95ddacb761aa75126112f323
svish <vishnupriyas248@gmail.com>
2024-05-18 16:42:30 +0530

    Initial commit

---
+++ .py
@@ -0,0 +1,44 @@
+def print_grid(grid):
+    for row in grid:
+        print(" ".join(str(num) if num != 0 else '.' for num in row))
+def is_valid(grid, row, col, num):
+    if num in grid[row]:
+        return False
+    if num in [grid[i][col] for i in range(9)]:
+        return False
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if grid[i][j] == num:
+                return False
+    return True
+def solve_sudoku(grid):
+    for row in range(9):
+        for col in range(9):
+            if grid[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(grid, row, col, num):
+                        grid[row][col] = num
+                        if solve_sudoku(grid):
+                            return True
+                        grid[row][col] = 0
+                return False
+    return True
+sudoku_grid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+print("Unsolved Sudoku:")
+print_grid(sudoku_grid)
+if solve_sudoku(sudoku_grid):
+    print("\nSolved Sudoku:")
+    print_grid(sudoku_grid)
+else:
+    print("No solution exists")
